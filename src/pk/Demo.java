cd7ac24b77c46bbf3723e20fcdc5e5c425025dd3
amsidhlokhande <amsidhlokhande@gmail.com>
2020-04-19 22:49:18 +0530

    Initial Commit

---
+++ src/pk/Demo.java
@@ -0,0 +1,15 @@
+package pk;
+
+import java.util.Date;
+
+public class Demo 
+{
+  @SuppressWarnings("deprecation")
+public static void main(String arg[])
+  {
+	  System.out.println("Hello From Test Application");
+	  Date date=new Date();
+	  System.out.println("Current Time"+date.toLocaleString());
+	  
+  }
+}
